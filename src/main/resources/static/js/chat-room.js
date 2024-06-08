cea547c516ef9f72cc4a5b8541dee94d0c34ae52
Ubuntu <ubuntu@ip-172-31-34-188.ap-northeast-2.compute.internal>
2024-06-08 13:26:45 +0000

    edit char-room.js - server-ip

--- src/main/resources/static/js/chat-room.js
+++ src/main/resources/static/js/chat-room.js
@@ -1,6 +1,6 @@
 const stompClient = new StompJs.Client({
     //web socket server 설정
-    brokerURL: 'ws://43.201.96.168:8080/endpoint'
+    brokerURL: 'ws://43.201.114.163:8080/endpoint'
 })
 
 stompClient.activate();
