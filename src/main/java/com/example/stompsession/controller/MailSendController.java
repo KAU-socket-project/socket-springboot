19d7dbd123fd907138d779d5c575e523ddb8d978
sebin chu <cntpqls0218@gmail.com>
2024-06-20 20:38:24 +0900

    [CHORE] 불필요한 import 제거

--- src/main/java/com/example/stompsession/controller/MailSendController.java
+++ src/main/java/com/example/stompsession/controller/MailSendController.java
@@ -7,12 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequiredArgsConstructor
